11c2aaf5ad43f1f9b73b6ee0820afa77a679865e
Frank Yang <frank840629@gmail.com>
2026-05-18 23:03:58 +0800

    ci: add github actions for deployment

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // base 的值必須是你的 GitHub 儲存庫（Repository）名稱，前後都要加上斜線
+  // 例如你的儲存庫叫 love-letter，這裡就寫 '/love-letter/'
+  base: '/love-letter/', 
+})
